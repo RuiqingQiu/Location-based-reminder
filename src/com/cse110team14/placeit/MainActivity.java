c14d1c751620f4370dabd5b220d94a0e37435cdc
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-02-03 20:21:09 -0800

    long click listen, alert dialog added

--- src/com/cse110team14/placeit/MainActivity.java
+++ src/com/cse110team14/placeit/MainActivity.java
@@ -5,6 +5,10 @@ import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -12,13 +16,18 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.support.v4.app.FragmentActivity;
 
@@ -44,19 +53,20 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     .add(new LatLng(37.35, -122.0)); // Closes the polyline.
 	
 	
-	
+	AlertDialog.Builder alert;
 	Location myCurrentLocation;
 	LocationClient myLocationClient;
+	final Context context = this;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpMapIfNeeded();
-        map.setMyLocationEnabled(true);
+      
         //An marker example
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
+        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg").snippet("Hello"));
         myLocationClient =  new LocationClient(this, this, this);
-       
+        map.setMyLocationEnabled(true);
         //Marker with window info
         Marker melbourne = map.addMarker(new MarkerOptions()
         .position(MELBOURNE)
@@ -69,6 +79,9 @@ GooglePlayServicesClient.OnConnectionFailedListener {
         //myCurrentLocation = myLocationClient.getLastLocation();
         //LatLng ME = new LatLng(myCurrentLocation.getLatitude(), myCurrentLocation.getLongitude());
         //Marker me = map.addMarker(new MarkerOptions().position(ME).title("My Position"));
+    	
+    	
+    	
     }
     
     /*
@@ -77,8 +90,60 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     @Override
     protected void onStart() {
         super.onStart();
+
+        
+        map.setOnMapLongClickListener(new OnMapLongClickListener()
+        {
+            @Override
+            public void onMapLongClick(LatLng point)
+	        {
+            	MarkerOptions position = new MarkerOptions().anchor(0.5f, 0.5f)
+	                      .position(point)
+	                      .title("Marker")
+	                      .snippet("latitude: " + point.latitude + " longtitude: " + point.longitude);
+            	Marker options = map.addMarker(position);
+            	
+	        }
+        });
+        map.setOnInfoWindowClickListener(new OnInfoWindowClickListener(){
+
+			@Override
+			public void onInfoWindowClick(final Marker m) 
+			{		
+				alert = new AlertDialog.Builder(context);
+		        
+		        alert.setTitle("Placeit information");
+	            alert.setMessage("Title: ");
+
+		        // Set an EditText view to get user input 
+		        final EditText input = new EditText(context);
+		        String title;
+		        alert.setView(input);
+		        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int whichButton) {
+			          String value = input.getText().toString();
+			          m.setTitle(value);
+		          }
+		        });
+
+		        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		          public void onClick(DialogInterface dialog, int whichButton) {
+		            // Canceled.
+		          }
+		        });
+		        alert.show();
+		        m.showInfoWindow();
+			}
+        	
+        });
         // Connect the client.
         myLocationClient.connect();
+        
+
+    }
+    
+    public void initializeAlert(){
+    	
     }
     /*
      * Called when the Activity is no longer visible.
