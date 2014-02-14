e24298f55a474de82bcc74842c609dc32a076b6d
Malcolm Samuel <m1samuel@ucsd.edu>
2014-02-14 03:30:20 -0800

    -Custom comparator now works with Calendar type -Alligned radio buttons and added one for no reposting

--- src/com/cse110team14/placeit/CustomComparator.java
+++ src/com/cse110team14/placeit/CustomComparator.java
@@ -6,14 +6,6 @@ public class CustomComparator implements Comparator<PlaceIt> {
 
 	@Override
 	public int compare(PlaceIt lhs, PlaceIt rhs) {
-		return Integer.valueOf(extractBackslashes(lhs)).compareTo(extractBackslashes(rhs));
-	}
-	public int extractBackslashes(PlaceIt aplaceit){
-		String date = aplaceit.getDateReminded();
-		date = date.replace("/", "");
-		String temp1 = date.substring(4);
-		String temp2 = date.substring(0,4);
-		date = temp1+temp2;
-		return Integer.parseInt(date);
+		return lhs.getDateRemindedToCalendar().compareTo(rhs.getDateRemindedToCalendar());
 	}
 }
