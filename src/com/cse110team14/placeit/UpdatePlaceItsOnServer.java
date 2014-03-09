6b87f24b157ed79c2af2e22f0d1f69062ec470f4
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-03-09 16:23:50 -0700

    Change name

--- src/com/cse110team14/placeit/UpdatePlaceItsOnServer.java
+++ src/com/cse110team14/placeit/UpdatePlaceItsOnServer.java
@@ -17,9 +17,11 @@ import org.apache.http.message.BasicNameValuePair;
 import android.util.Log;
 
 import com.cse110team14.placeit.controller.MapOnClickController;
+import com.cse110team14.placeit.model.CPlaceIts;
 import com.cse110team14.placeit.model.PlaceIt;
 
 public class UpdatePlaceItsOnServer {
+	
 	public static void postPlaceIts(PlaceIt p) {
 		final PlaceIt tmp = p;
 		Thread t = new Thread() {
@@ -49,6 +51,57 @@ public class UpdatePlaceItsOnServer {
 			    		  Integer.toString(tmp.getPlaceItType())));
 			      nameValuePairs.add(new BasicNameValuePair("sneezeType",
 			    		  Integer.toString(tmp.getSneezeType())));
+			      nameValuePairs.add(new BasicNameValuePair("user",
+			    		  LoginActivity.loginActivity.username));
+			      nameValuePairs.add(new BasicNameValuePair("listType",
+			    		  tmp.getListType()));
+			      nameValuePairs.add(new BasicNameValuePair("action",
+				          "put"));
+			      post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			 
+			      HttpResponse response = client.execute(post);
+			      BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			      String line = "";
+			      while ((line = rd.readLine()) != null) {
+			        Log.d("hello", line);
+			      }
+
+			    } catch (IOException e) {
+			    	Log.d("hello", "IOException while trying to conect to GAE");
+			    }
+			}
+		};
+		t.start();
+	}
+	
+	public static void postCPlaceIts(CPlaceIts p) {
+		final CPlaceIts tmp = p;
+		Thread t = new Thread() {
+
+			public void run() {
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost(MapOnClickController.PLACEITS_URL);
+
+			    try {
+			      List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+			      nameValuePairs.add(new BasicNameValuePair("title",
+			    		  tmp.getTitle()));
+			      nameValuePairs.add(new BasicNameValuePair("description",
+			    		  tmp.getDescription()));
+			      nameValuePairs.add(new BasicNameValuePair("postDate",
+			    		  tmp.getDate()));
+			      nameValuePairs.add(new BasicNameValuePair("dateToBeReminded",
+			    		  tmp.getDateReminded()));
+			      //This is always a category placeit
+			      nameValuePairs.add(new BasicNameValuePair("type",
+			    		  "2"));
+			      String categories = "";
+			      String [] tmpArray = tmp.getCategories();
+			      for(String i : tmpArray){
+			    	  categories = categories + i + "###";
+			      }
+			      nameValuePairs.add(new BasicNameValuePair("categories", 
+			    		  categories));
 			      nameValuePairs.add(new BasicNameValuePair("user",
 			    		  LoginActivity.loginActivity.username));
 			      nameValuePairs.add(new BasicNameValuePair("listType",
